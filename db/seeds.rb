9dd71c0804564676d5ee913b0ef737a608b261a2
victoria husemeyer <victoria_husemeyer@yahoo.com>
2022-11-17 16:41:48 +0000

    lists done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts "Cleaning database"
+Movie.destroy_all
+
+puts "Creating movies"
+
+20.times do
+  movie = Movie.create(
+    title: Faker::Movie.title,
+    overview: Faker::Quote.famous_last_words,
+    poster_url: Faker::Internet.url,
+    rating: rand(1..10)
+  )
+
+  puts "Movie with id: #{movie.id} has been created"
+end
+
+puts "Finished"
