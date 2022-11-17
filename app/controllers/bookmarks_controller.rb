8842425d83608e4ed777c8f4e8c4c423d75f13ef
victoria husemeyer <victoria_husemeyer@yahoo.com>
2022-11-17 17:45:59 +0000

    added create bookmark

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,31 @@
 class BookmarksController < ApplicationController
+
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+    # @movies = Movie.all
+    #@movies = Movie.where.not(id: @list.movies).order(:title)
+
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @movie = Movie.find(params[:bookmark][:movie_id])
+
+    @bookmark.movie = @movie
+    @bookmark.list = @list
+
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
+end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :lists do
-    resources :bookmark, only: [:new, :create]
-  end
-end
+  #root to: 'lists#index'
+#only: [ :show, :new, :create]
